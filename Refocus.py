3c095bac6fc32a10096dca246ffa39dbe810cf10
LaoshWei <b07611011@ntu.edu.tw>
2022-04-12 15:50:35 +0800

    Complete the annotations

--- Refocus.py
+++ Refocus.py
@@ -1,36 +1,37 @@
 import cv2
 import numpy as np
 from pathlib import Path
-def refocus(Light_field, patch_len, factor = 1, a = 1):
+def refocus(Light_field, num_views, factor = 1, a = 1):
     '''
-    factor : the factor that used to scale the image
+    Light_field : light field images array (It should contain num_views*num_views images)
+    num_views : the number of views each rows or columns
+    factor : the parameter which is used to scale the image
+    a : focal length (It can be negative)
     '''
     size = (int(Light_field[0, 0].shape[1]*factor),int(Light_field[0, 0].shape[0]*factor))
-    overlap = int(abs(a) * (patch_len - 1))
+    overlap = int(abs(a) * (num_views - 1))
     img_slice = np.zeros((size[1]+overlap,size[0]+overlap,3))
-    for j in range(patch_len):
-        for i in range(patch_len):
-            # perform sub-pixel refinement if required
+    for j in range(num_views):
+        for i in range(num_views):
+            # Because we are going to add all image together, each image should be divide by num_views ** 2
             if factor != 1:
-                vp_img = cv2.resize(Light_field[j, i],size) / (patch_len **2)
+                vp_img = cv2.resize(Light_field[j, i],size) / (num_views **2)
             else:
-                vp_img = Light_field[j, i] / 25 #(patch_len ** 2) 
-                
+                vp_img = Light_field[j, i] / (num_views ** 2) 
                 
             # get viewpoint padding for each border
-            tb = (int(abs(a) * j), int(abs(a) * (patch_len - 1 - j)))    # top, bottom
-            lr = (int(abs(a) * i), int(abs(a) * (patch_len - 1 - i)))    # left, right
+            tb = (int(abs(a) * j), int(abs(a) * (num_views - 1 - j)))    # top, bottom
+            lr = (int(abs(a) * i), int(abs(a) * (num_views - 1 - i)))    # left, right
 
             # flip padding for each axis if a is negative
             pad_width = (tb, lr, (0, 0)) if a >= 0 else (tb[::-1], lr[::-1], (0, 0))
             # shift viewpoint image and add its values to refocused image slice
             img_slice = np.add(img_slice, np.pad(vp_img, pad_width, 'edge'))
-            # print(img_slice)
 
     # crop refocused image for consistent image dimensions
-    img_slice=np.array(img_slice)
     crop = int(overlap/2)
     final_img = img_slice[crop:-crop, crop:-crop, :] if (a != 0) else img_slice
     final_img = cv2.resize(img_slice,(Light_field[0, 0].shape[1],Light_field[0, 0].shape[0]))
+    final_img = final_img.astype(np.uint8)
 
     return final_img
--- main.py
+++ main.py
@@ -3,20 +3,23 @@ import cv2
 import numpy as np
 from Refocus import refocus
 
-patch_len = 5
+num_views = 5
 count = 0
 a= 0
 factor = 1
+
+# Reminds that Views of the light field should be arranges from top to the bottom and from left to the right
+# This dataset is arragned from bottom to the top, so I reverse it
 images = []
 files = Path(r"DragonAndBunnies\DragonsAndBunnies_5x5_ap6.6").glob("*.png")
 for file in files:
     img = cv2.imread(str(file))
-    if count % patch_len == 0:
+    if count % num_views == 0:
         images.append([])
-    images[count // patch_len].append(img)
+    images[count // num_views].append(img)
     count += 1
 images.reverse()
 Light_Field = np.array(images)
 
-Final_img = refocus(Light_Field, patch_len, factor, a)
+Final_img = refocus(Light_Field, num_views, factor, a)
 cv2.imwrite(fr"Test.jpg",Final_img)
